72170ae1fe54c316eb688c4ea8d42e4a522724d9
louisgjohnson <louisgjohnson@gmail.com>
2020-06-12 08:41:42 +1000

    updates

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,29 @@
 use magma_gfx::init_renderer;
 use magma_gfx::shapes::Rectangle;
 use magma_gfx::core::Color;
+use magma_gfx::textures::Texture2D;
+
+extern crate nalgebra_glm as glm;
+
+
 
 fn main() {
-    let (gfx, mut window) = init_renderer();
-    let size = winit::dpi::LogicalSize::new(1200, 800);
-    window = window.with_inner_size(size).with_title("Testing vulkan");
+    let (mut gfx, window) = init_renderer();
+    // let size = winit::dpi::LogicalSize::new(1200, 800);
     let rect = Rectangle {
-        position: cgmath::Vector2 {
-            x: 4.,
-            y: 5.
-        },
+        position: nalgebra_glm::Vec2::new(0., 0.),
         width: 50,
         height: 50
     };
 
     let arr: [u8; 3] = [150, 155, 250];
 
+    let mut texture = Texture2D::load(&mut gfx, "./test.png".to_string());
+
     
     gfx.run(window, move |draw| {
-        draw.rectangle(&rect, Color::new(arr[0], arr[1], arr[2]));
+        draw.rectangle(&rect, Color::new(50, 50, 50));
+        draw.texture_pro(&mut texture,  nalgebra_glm::Vec2::new(600., 400.), 5.);
     });
 }
 
